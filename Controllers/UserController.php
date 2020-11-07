4a91a4cac7773dd3ee44afb26ff8d835abcb6845
achrafOuk <achraf.crispy@gmail.com>
2020-11-07 01:30:52 +0100

    working on authenfication system

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -4,9 +4,14 @@ namespace app\Controllers;
 
 class UserController 
 {
-    public function login()
+    public function login($argms=[])
     {
         //do something
+        if ( $argms!=[] ) 
+        {
+            echo "404!";
+            die();
+        }
         $file = dirname(__DIR__).DIRECTORY_SEPARATOR ."Views".DIRECTORY_SEPARATOR ."users".DIRECTORY_SEPARATOR ."login.php";
         include_once($file);
     }
--- Views/users/login.php
+++ Views/users/login.php
@@ -1,1 +1,27 @@
-<p>Login</p>
+<html>
+<head>
+<title>
+    Store - Home page
+</title>
+<?php
+$includes = dirname(__DIR__).DIRECTORY_SEPARATOR ."components".DIRECTORY_SEPARATOR ;
+$navbar = $includes . "bootstrap.php";
+include_once $navbar;
+?>
+<head>
+<body>
+<?php
+include_once $includes."navbar.php";
+
+?>
+<section id="login">
+<p>Login page</p>
+</section>
+
+
+<?php
+include_once $includes."footer.php";
+
+?>
+</body>
+</html>
