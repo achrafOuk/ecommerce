9d91a240e3ef01bc095e13bcd735c22b662fb1cd
achrafOuk <achraf.crispy@gmail.com>
2020-12-14 16:51:23 +0100

    make item number changeble

--- public/js/items.js
+++ public/js/items.js
@@ -17,8 +17,10 @@ function mergeduplicateelement() {
       }
     }
     let isExists = false;
-    for (k in storageCopie) {
-      if (Object.is(JSON.stringify(storageCopie[k]), JSON.stringify(item))) {
+    for (k in window.storageCopie) {
+      if (
+        Object.is(JSON.stringify(window.storageCopie[k]), JSON.stringify(item))
+      ) {
         isExists = true;
       }
     }
@@ -28,15 +30,18 @@ function mergeduplicateelement() {
   }
   return storageCopie;
 }
-function addElementToPanier() {
+window.storage = mergeduplicateelement();
+function addElementToPanier(dataItem) {
   let i;
   let panier = document.querySelector("#panier");
   let Totalprice = document.querySelector("#price");
   let Price = 0;
   panier.innerHTML = "";
-  let storage = mergeduplicateelement();
-  for (i in storage) {
-    let Item = storage[i];
+  window.storage = dataItem;
+  console.log(window.storage);
+  for (i in window.storage) {
+    console.log(window.storage[i]);
+    let Item = window.storage[i];
     let div = document.createElement("div");
     div.classList.add("itemPanier");
     let img = document.createElement("img");
@@ -46,6 +51,7 @@ function addElementToPanier() {
     title.href = Item["link"];
     title.classList.add("title");
     let quality = Item["quality"];
+    console.log("item:", Item["quality"]);
     let Quality = document.createElement("input");
     Quality.setAttribute("type", "number");
     Quality.setAttribute("min", "1");
@@ -64,26 +70,38 @@ function addElementToPanier() {
     div.appendChild(price);
     div.appendChild(deleteButton);
     panier.appendChild(div);
-
-    //   paniier.appendChild()
   }
   Totalprice.innerText = Price;
 }
 
-addElementToPanier();
-document.querySelectorAll(".quality").forEach((item, index) => {
-  item.addEventListener("change", (event) => {
-    let sum = 0;
-    let items = document.querySelectorAll(".itemPanier");
-    console.log(items.length);
-    for (let i = 0; i < items.length; i++) {
-      let quanitity = document.querySelectorAll(".quality")[i].value;
-      let price = document
-        .querySelectorAll(".itemPanier p")
-        [i].innerHTML.split(" ")[0];
-      price = parseInt(price, 10);
-      sum += price * quanitity;
-    }
-    let total = (document.querySelector("#price").innerHTML = sum);
+console.log(window.storage);
+addElementToPanier(window.storage);
+console.log(window.storage);
+
+function fun() {
+  document.querySelectorAll(".quality").forEach((item, index) => {
+    item.addEventListener("change", (event) => {
+      let sum = 0;
+      console.log(window.storage);
+      let items = document.querySelectorAll(".itemPanier");
+      window.storage[index].quality = document.querySelectorAll(".quality")[
+        index
+      ].value;
+      console.log("item1:", window.storage[index].quality);
+      for (let i = 0; i < items.length; i++) {
+        let quanitity = document.querySelectorAll(".quality")[i].value;
+        let price = document
+          .querySelectorAll(".itemPanier p")
+          [i].innerHTML.split(" ")[0];
+        price = parseInt(price, 10);
+        sum += price * quanitity;
+      }
+      let total = (document.querySelector("#price").innerHTML = sum);
+      addElementToPanier(window.storage);
+      console.log(window.storage);
+      fun();
+      console.log(window.storage[index].quality);
+    });
   });
-});
+}
+fun();
